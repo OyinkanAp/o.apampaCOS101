6f1b9bdffad86370a1d79ef44807ba417ba63835
OyinkanAp <oyinkansola.apampa@pau.edu.ng>
2025-11-05 17:26:27 +0100

    Delete Questin2-SLRPE directory

--- Questin2-SLRPE/target/debug/Questin2-SLRPE.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Owner\OneDrive\Documents\o.apampaCOS101\Questin2-SLRPE\target\debug\Questin2-SLRPE.exe: C:\Users\Owner\OneDrive\Documents\o.apampaCOS101\Questin2-SLRPE\src\main.rs
--- Questin2-SLRPE/target/debug/deps/Questin2_SLRPE.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Owner\OneDrive\Documents\o.apampaCOS101\Questin2-SLRPE\target\debug\deps\Questin2_SLRPE.d: src\main.rs
-
-C:\Users\Owner\OneDrive\Documents\o.apampaCOS101\Questin2-SLRPE\target\debug\deps\Questin2_SLRPE.exe: src\main.rs
-
-src\main.rs:
